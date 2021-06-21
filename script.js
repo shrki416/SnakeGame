cacf855c52eb1c7c112ff1f58b4667f915cf124d
nathan-mccraw <naterltw175@gmail.com>
2021-06-20 22:04:51 -0400

    moving/controllable square on the canvas.  Start by pressing space

--- script.js
+++ script.js
@@ -1,9 +1,94 @@
-window.onload = () => {
-  let FPS = 30;
-  setInterval(drawEverything, 1000 / FPS);
+document.addEventListener("keydown", whichKeyisPressed);
+
+const snake = {
+  Length: 10,
+  SpeedinX: 0,
+  SpeedinY: 5,
+  PositioninX: 400,
+  PositioninY: 250,
+  selectedSpeedPositive: 5,
+  selectedSpeedNegative: -5,
+  pausedIntervalID: 1,
 };
 
-function drawEverything() {
+window.onload = () => {
+  initializeGame();
+  //   let FPS = 30;
+  //   setInterval(drawEverything, 1000 / FPS);
+};
+
+function whichKeyisPressed(e) {
+  const keyPressed = e.key;
+
+  if (keyPressed === "ArrowDown" || keyPressed === "s") turnSnakeDown();
+  if (keyPressed === "ArrowUp" || keyPressed === "w") turnSnakeUp();
+  if (keyPressed === "ArrowLeft" || keyPressed === "a") turnSnakeLeft();
+  if (keyPressed === "ArrowRight" || keyPressed === "d") turnSnakeRight();
+  if (keyPressed === " ") toggleGamePause();
+}
+
+function toggleGamePause() {
+  //   if (!snake.pausedIntervalID) {
+  snake.pausedIntervalID = setInterval(() => {
+    drawEverythingElse();
+    drawSnake();
+  }, 200);
+  //   } else clearInterval(pausedIntervalID);
+}
+
+function turnSnakeDown() {
+  if (snake.SpeedinY === 0) {
+    snake.SpeedinX = 0;
+    snake.SpeedinY = snake.selectedSpeedPositive;
+  }
+
+  drawSnake();
+}
+
+function turnSnakeUp() {
+  if (snake.SpeedinY === 0) {
+    snake.SpeedinX = 0;
+    snake.SpeedinY = snake.selectedSpeedNegative;
+  }
+
+  drawSnake();
+}
+
+function turnSnakeLeft() {
+  if (snake.SpeedinX === 0) {
+    snake.SpeedinY = 0;
+    snake.SpeedinX = snake.selectedSpeedNegative;
+  }
+
+  drawSnake();
+}
+
+function turnSnakeRight() {
+  if (snake.SpeedinX === 0) {
+    snake.SpeedinY = 0;
+    snake.SpeedinX = snake.selectedSpeedPositive;
+  }
+
+  drawSnake();
+}
+
+function drawSnake() {
+  const canvas = document.querySelector("#gameCanvas");
+  const canvasContext = canvas.getContext("2d");
+
+  snake.PositioninY += snake.SpeedinY;
+  snake.PositioninX += snake.SpeedinX;
+
+  canvasContext.fillStyle = "green";
+  canvasContext.fillRect(
+    snake.PositioninX,
+    snake.PositioninY,
+    10,
+    snake.Length
+  );
+}
+
+function drawEverythingElse() {
   const canvas = document.querySelector("#gameCanvas");
   const canvasContext = canvas.getContext("2d");
 
@@ -11,5 +96,19 @@ function drawEverything() {
   canvas.height = 500;
 
   canvasContext.fillStyle = "black";
-  canvasContext.fillRect(0, 0, 800, 500);
+  canvasContext.fillRect(0, 0, canvas.width, canvas.height);
+}
+
+function initializeGame() {
+  const canvas = document.querySelector("#gameCanvas");
+  const canvasContext = canvas.getContext("2d");
+
+  canvas.width = 800;
+  canvas.height = 500;
+
+  canvasContext.fillStyle = "black";
+  canvasContext.fillRect(0, 0, canvas.width, canvas.height);
+
+  canvasContext.fillStyle = "green";
+  canvasContext.fillRect(400, 250, 10, snake.Length);
 }
