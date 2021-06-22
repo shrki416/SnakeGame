cbdd342115d3d6053671c4676b295e5cb675434c
nathan-mccraw <naterltw175@gmail.com>
2021-06-22 00:16:43 -0400

    randomized location of apple; implemented collision with 'snake' dot and 'apple' dot

--- script.js
+++ script.js
@@ -1,71 +1,81 @@
-document.addEventListener("keydown", whichKeyisPressed);
-
 const snake = {
-  Length: 10,
-  SpeedinX: 0,
-  SpeedinY: 1,
-  PositioninX: 400,
-  PositioninY: 250,
+  speedInX: 0,
+  speedInY: 1,
+  positionX: 400,
+  positionY: 250,
+};
+
+const apple = {
+  positionX: 0,
+  positionY: 0,
+  size: 10,
+  numberEaten: 0,
+};
+
+const gameSettings = {
+  canvasWidth: 600,
+  canvasHeight: 500,
   selectedSpeed: 1,
   isGameRunning: 0,
 };
 
 window.onload = () => {
   initializeGame();
-  //   let FPS = 30;
-  //   setInterval(drawEverything, 1000 / FPS);
 };
+
+document.addEventListener("keydown", whichKeyisPressed);
 
 function whichKeyisPressed(e) {
   const keyPressed = e.key;
 
   if (keyPressed === " ") toggleGamePause();
-  if (snake.isGameRunning > 0) {
-    if (keyPressed === "ArrowDown" || keyPressed === "s") turnSnakeDown();
+  if (gameSettings.isGameRunning > 0) {
     if (keyPressed === "ArrowUp" || keyPressed === "w") turnSnakeUp();
+    if (keyPressed === "ArrowDown" || keyPressed === "s") turnSnakeDown();
     if (keyPressed === "ArrowLeft" || keyPressed === "a") turnSnakeLeft();
     if (keyPressed === "ArrowRight" || keyPressed === "d") turnSnakeRight();
   }
 }
 
 function toggleGamePause() {
-  if (snake.isGameRunning) {
-    clearInterval(snake.isGameRunning);
-    snake.isGameRunning = 0;
+  if (gameSettings.isGameRunning) {
+    clearInterval(gameSettings.isGameRunning);
+    gameSettings.isGameRunning = 0;
   } else {
-    snake.isGameRunning = setInterval(() => {
+    gameSettings.isGameRunning = setInterval(() => {
       drawEverythingElse();
       drawSnake();
       isSnakeInbounds();
-    }, 20);
+      didEatApple();
+    }, 25);
   }
 }
 
 function turnSnakeDown() {
-  if (snake.SpeedinY === 0) {
-    snake.SpeedinX = 0;
-    snake.SpeedinY = snake.selectedSpeed;
+  if (snake.speedInY === 0) {
+    snake.speedInX = 0;
+    snake.speedInY = gameSettings.selectedSpeed;
   }
 }
 
 function turnSnakeUp() {
-  if (snake.SpeedinY === 0) {
-    snake.SpeedinX = 0;
-    snake.SpeedinY = -snake.selectedSpeed;
+  if (snake.speedInY === 0) {
+    snake.speedInX = 0;
+    snake.speedInY = -gameSettings.selectedSpeed;
   }
 }
 
 function turnSnakeLeft() {
-  if (snake.SpeedinX === 0) {
-    snake.SpeedinY = 0;
-    snake.SpeedinX = -snake.selectedSpeed;
+  if (snake.speedInX === 0) {
+    snake.speedInY = 0;
+    snake.speedInX = -gameSettings.selectedSpeed;
   }
 }
 
 function turnSnakeRight() {
-  if (snake.SpeedinX === 0) {
-    snake.SpeedinY = 0;
-    snake.SpeedinX = snake.selectedSpeed;
+  if (snake.speedInX === 0) {
+    snake.speedInY = 0;
+    snake.speedInX = gameSettings.selectedSpeed;
   }
 }
 
@@ -73,54 +83,84 @@ function drawSnake() {
   const canvas = document.querySelector("#gameCanvas");
   const canvasContext = canvas.getContext("2d");
 
-  snake.PositioninY += snake.SpeedinY;
-  snake.PositioninX += snake.SpeedinX;
+  snake.positionY += snake.speedInY;
+  snake.positionX += snake.speedInX;
 
   canvasContext.fillStyle = "green";
-  canvasContext.fillRect(
-    snake.PositioninX,
-    snake.PositioninY,
-    10,
-    snake.Length
-  );
+  canvasContext.fillRect(snake.positionX, snake.positionY, 10, 10);
 }
 
 function drawEverythingElse() {
   const canvas = document.querySelector("#gameCanvas");
   const canvasContext = canvas.getContext("2d");
 
-  canvas.width = 800;
-  canvas.height = 500;
+  canvas.width = gameSettings.canvasWidth;
+  canvas.height = gameSettings.canvasHeight;
 
   canvasContext.fillStyle = "black";
   canvasContext.fillRect(0, 0, canvas.width, canvas.height);
+
+  canvasContext.fillStyle = "red";
+  canvasContext.fillRect(
+    apple.positionX,
+    apple.positionY,
+    apple.size,
+    apple.size
+  );
 }
 
 function initializeGame() {
   const canvas = document.querySelector("#gameCanvas");
   const canvasContext = canvas.getContext("2d");
 
-  canvas.width = 800;
+  canvas.width = 600;
   canvas.height = 500;
+  snake.positionX = canvas.width / 2;
+  snake.positionY = canvas.height / 2;
 
   canvasContext.fillStyle = "black";
   canvasContext.fillRect(0, 0, canvas.width, canvas.height);
 
   canvasContext.fillStyle = "green";
-  canvasContext.fillRect(400, 250, 10, snake.Length);
+  canvasContext.fillRect(snake.positionX, snake.positionY, 10, 10);
+
+  placeApple();
 }
 
 function isSnakeInbounds() {
-  if (snake.PositioninX === 0 || snake.PositioninX > 800) resetGame();
-  if (snake.PositioninY === 0 || snake.PositioninY > 500) resetGame();
+  if (snake.positionX === 0 || snake.positionX > gameSettings.canvasWidth - 10)
+    resetGame();
+  if (snake.positionY === 0 || snake.positionY > gameSettings.canvasHeight - 10)
+    resetGame();
+}
+
+function didEatApple() {
+  if (
+    snake.positionY >= apple.positionY &&
+    snake.positionY <= apple.positionY + apple.size
+  ) {
+    if (
+      snake.positionX >= apple.positionX &&
+      snake.positionX <= apple.positionX + apple.size
+    ) {
+      apple.numberEaten++;
+      placeApple();
+    }
+  }
 }
 
 function resetGame() {
-  snake.SpeedinX = 0;
-  snake.SpeedinY = -snake.selectedSpeed;
-  snake.Length = 10;
-  snake.PositioninX = 400;
-  snake.PositioninY = 250;
+  snake.speedInX = 0;
+  snake.speedInY = -gameSettings.selectedSpeed;
+  snake.positionX = gameSettings.canvasWidth / 2;
+  snake.positionY = gameSettings.canvasHeight / 2;
   toggleGamePause();
   initializeGame();
+}
+
+function placeApple() {
+  apple.positionX = Math.floor(Math.random() * (gameSettings.canvasWidth - 10));
+  apple.positionY = Math.floor(
+    Math.random() * (gameSettings.canvasHeight - 10)
+  );
 }
